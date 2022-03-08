63e697cc86b8634c2d9d9be174837dd7e4b19b76
Rafa hhll <hola@rafahhll.com>
2022-03-08 12:38:01 -0600

    initial commit

---
+++ splashScreen/SplashScreen.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace splashScreen
+{
+    partial class FrmSplasScreen
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmSplasScreen));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pbLoading = new System.Windows.Forms.ProgressBar();
+            this.tLoading = new System.Windows.Forms.Timer(this.components);
+            this.lDescripionProgreso = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(212, 108);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(250, 58);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(153, 43);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(234, 205);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Estamo preparando todo para ti";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(562, 205);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(110, 110);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(409, 363);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(265, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "2018 - 2021 Todos los derechos reservados";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(12, 363);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(232, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Un producto de https://8020digital.net";
+            // 
+            // pbLoading
+            // 
+            this.pbLoading.Location = new System.Drawing.Point(15, 292);
+            this.pbLoading.Maximum = 10;
+            this.pbLoading.Name = "pbLoading";
+            this.pbLoading.Size = new System.Drawing.Size(547, 23);
+            this.pbLoading.TabIndex = 6;
+            // 
+            // tLoading
+            // 
+            this.tLoading.Enabled = true;
+            this.tLoading.Interval = 1000;
+            this.tLoading.Tick += new System.EventHandler(this.tLoading_Tick);
+            // 
+            // lDescripionProgreso
+            // 
+            this.lDescripionProgreso.AutoSize = true;
+            this.lDescripionProgreso.Font = new System.Drawing.Font("Yu Gothic UI", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lDescripionProgreso.ForeColor = System.Drawing.Color.White;
+            this.lDescripionProgreso.Location = new System.Drawing.Point(12, 266);
+            this.lDescripionProgreso.Name = "lDescripionProgreso";
+            this.lDescripionProgreso.Size = new System.Drawing.Size(123, 13);
+            this.lDescripionProgreso.TabIndex = 7;
+            this.lDescripionProgreso.Text = "descripcion de progreso";
+            // 
+            // FrmSplasScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(26)))), ((int)(((byte)(43)))));
+            this.ClientSize = new System.Drawing.Size(684, 389);
+            this.Controls.Add(this.lDescripionProgreso);
+            this.Controls.Add(this.pbLoading);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmSplasScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bienvenido";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar pbLoading;
+        private System.Windows.Forms.Timer tLoading;
+        private System.Windows.Forms.Label lDescripionProgreso;
+    }
+}
+
---
+++ splashScreen/SplashScreen.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace splashScreen
+{
+    public partial class FrmSplasScreen : Form
+    {
+        int contador = 0;
+
+        List<string> list_descripcion_progreso = new List<string>
+        {
+            "Conectando con el servidor...",
+            "Verificando nuevas versiones...",
+            "Cargando información de la base de datos...",
+            "Validando licencia del producto...",
+            "Estamos terminando..",
+            "La paciencia es una virtud...",
+            "Te cuento un chiste...",
+            "Ahi va.... ",
+            "A no te creas ya terminamos...",
+            "Muchas gracias por la espera ; ) ..."
+
+        };
+
+        public FrmSplasScreen()
+        {
+            InitializeComponent();
+            lDescripionProgreso.Text = "";
+        }
+
+        private void tLoading_Tick(object sender, EventArgs e)
+        {
+            loadProgressBar(contador);
+            contador++;
+
+            if (contador == 10)
+            {
+                pbLoading.Value = 10;
+                Application.Exit();
+            }
+        }
+
+        public void loadProgressBar(int increment)
+        {
+            lDescripionProgreso.Text = list_descripcion_progreso[increment];
+
+            pbLoading.Value = (increment + 1);
+        }
+    }
+}
